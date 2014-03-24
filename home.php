47f3b8883d0e0f888659018dcb66296948d13d76
ItsJonQ <itsjonq@gmail.com>
2014-03-23 22:43:40 -0400

    Working on building styles for sections

--- home.php
+++ home.php
@@ -8,15 +8,30 @@
 get_header(); ?>
 
 <!-- Section: Hello -->
-<section id="section-hello" class="featurette shaded hello">
+<section id="section-hello" class="featurette section shaded hello">
 
     <div class="container">
 
-        <h2 class="headline"><strong>Hello</strong>. I'm Q!</h2>
+        <div class="col-md-8 col-solo">
+
+            <h2 class="headline"><strong>Hello</strong>. I'm Q!</h2>
+            <h3 class="sub-headline">I'm a web designer/developer who's really into UI/UX, simplicity, <br />and making things <em>pretty</em>.</h3>
+
+        </div>
 
     </div>
 
 </section>
 <!-- / #section-hello -->
+
+<section class="section">
+
+    <dic class="container">
+
+        <h2 class="section-title">Section Title</h2>
+
+    </div>
+
+</section>
 
 <?php get_footer(); ?>
