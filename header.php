3b9e60da21c631068d52561e7e549ed00dd21dc0
ItsJonQ <itsjonq@gmail.com>
2014-03-23 21:01:41 -0400

    Added scripts+styles to load with init hook:

--- header.php
+++ header.php
@@ -11,6 +11,7 @@
 <html <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
     <title><?php wp_title( '|', true, 'right' ); ?></title>
     <link rel="profile" href="http://gmpg.org/xfn/11">
@@ -24,16 +25,12 @@
 
         <header id="masthead" class="site-header" role="banner">
             <div class="site-branding">
-                <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-                <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+                <h1 class="site-title">
+                    <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+                        <?php bloginfo( 'name' ); ?>
+                    </a>
+                </h1>
             </div>
-
-            <nav id="site-navigation" class="main-navigation" role="navigation">
-                <h1 class="menu-toggle"><?php _e( 'Menu', '_s' ); ?></h1>
-                <a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', '_s' ); ?></a>
-
-                <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-            </nav>
         </header>
         <!-- / #masthead -->
 
--- lib/classes/class.q_.php
+++ lib/classes/class.q_.php
@@ -11,12 +11,49 @@ if(!class_exists('q_')) {
 
         }
 
+        // fn: Header - Meta Viewport
         public static function header_meta_viewport() {
             echo "\n" . '<meta name="viewport" content="width=device-width, initial-scale=1">' . "\n\n";
+        }
+
+        // fn: Header - Custom Scripts
+        public static function header_custom_scripts() {
+
+            $lib = get_stylesheet_directory_uri() . '/lib/';
+            $public = get_stylesheet_directory_uri() . '/public/';
+            $vendor = get_stylesheet_directory_uri() . '/vendor/';
+
+            // Scripts
+            wp_enqueue_script('jquery', $vendor . 'js/jquery.min.js', array(), '20140323', true);
+            wp_enqueue_script('underscore', $vendor . 'js/underscore.min.js', array(), '20140323', true);
+            wp_enqueue_script('knockout', $vendor . 'js/knockout.min.js', array(), '20140323', true);
+
+            wp_enqueue_script('q-main-js', $public . 'js/main.js', array('jquery'), '20140323', true);
+
+            // Styles
+
+            // Resources
+            wp_register_style( 'bootstrap', $vendor . 'css/bootstrap.min.css', array(), null);
+
+            // Main
+            wp_register_style( 'q-main-css', $oublic . 'css/main.css', array('bootstrap'), null);
+
+            wp_enqueue_style( 'bootstrap' );
+            wp_enqueue_style( 'q-main-css' );
+
+        }
+
+        public static function header_custom_scripts_init() {
+            // Deregister jQuery if the page is not Admin
+            if (!is_admin()) wp_deregister_script('jquery');
+
+            // Adding the action to the init function
+            add_action('wp_enqueue_scripts', array( 'q_', 'header_custom_scripts' ));
         }
 
     }
 
     add_action( 'init' , array( 'q_' , 'init' ) );
+    add_action( 'init' , array( 'q_', 'header_custom_scripts_init' ) );
 
 }
