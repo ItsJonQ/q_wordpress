9057840183756353f74d57280cabbfaab86652cb
ItsJonQ <itsjonq@gmail.com>
2014-04-12 23:40:37 -0400

    Created the q_post class

---
+++ lib/classes/class.q_post.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * q_post
+ * This is the post class for the q_ wordpress theme
+ *
+ * @class
+ *
+ * @package q_
+ *
+ */
+
+// Defining the q_post class
+if(!class_exists('q_post')) {
+
+    class q_post {
+
+        /**
+         * init
+         * Initialization method for this class
+         *
+         * @init
+         */
+        public static function init() {
+            /**
+             * Nothin' yet!
+             */
+        }
+
+    }
+
+    // Initialize
+    // add_action( 'init' , array( 'q_post' , 'init' ) );
+
+}
