eed7fddfdb94d1969dbd7f0bf4201d0baef1d26f
ItsJonQ <itsjonq@gmail.com>
2014-04-12 23:41:33 -0400

    Created a sample template for q classes

---
+++ lib/classes/class.q_sample.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * q_sample
+ * This is the sample class for the q_ wordpress theme
+ *
+ * @class
+ *
+ * @package q_
+ *
+ */
+
+// Defining the q_sample class
+if(!class_exists('q_sample')) {
+
+    class q_sample {
+
+        /**
+         * init
+         * Initialization method for this class
+         *
+         * @init
+         */
+        public static function init() {
+            /**
+             * Nothin' yet!
+             */
+        }
+
+    }
+
+    // Initialize
+    // add_action( 'init' , array( 'q_sample' , 'init' ) );
+
+}
