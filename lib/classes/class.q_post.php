9eb87b135cce1b1777da18ad3534de624d2140a1
ItsJonQ <itsjonq@gmail.com>
2014-04-13 15:38:00 -0400

    Adjusted titles for archive pages. Added hashtag for tags

--- lib/classes/class.q_post.php
+++ lib/classes/class.q_post.php
@@ -38,10 +38,10 @@ if(!class_exists('q_post')) {
         public static function archive_title() {
 
             if ( is_category() ) :
-                single_cat_title();
+                printf( '<strong>' . single_cat_title( '', false ) . '</strong>' );
 
             elseif ( is_tag() ) :
-                single_tag_title();
+                printf( '<span class="hashtag">#</span><strong>' . single_tag_title( '', false ) . '</strong>' );
 
             elseif ( is_author() ) :
                 printf( __( 'Posts by <strong>%s</strong>', '_s' ), '<span class="vcard">' . get_the_author() . '</span>' );
@@ -55,32 +55,32 @@ if(!class_exists('q_post')) {
             elseif ( is_year() ) :
                 printf( __( 'Year: %s', '_s' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', '_s' ) ) . '</span>' );
 
-            elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
-                _e( 'Asides', '_s' );
+            // elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
+            //     _e( 'Asides', '_s' );
 
-            elseif ( is_tax( 'post_format', 'post-format-gallery' ) ) :
-                _e( 'Galleries', '_s');
+            // elseif ( is_tax( 'post_format', 'post-format-gallery' ) ) :
+            //     _e( 'Galleries', '_s');
 
-            elseif ( is_tax( 'post_format', 'post-format-image' ) ) :
-                _e( 'Images', '_s');
+            // elseif ( is_tax( 'post_format', 'post-format-image' ) ) :
+            //     _e( 'Images', '_s');
 
-            elseif ( is_tax( 'post_format', 'post-format-video' ) ) :
-                _e( 'Videos', '_s' );
+            // elseif ( is_tax( 'post_format', 'post-format-video' ) ) :
+            //     _e( 'Videos', '_s' );
 
-            elseif ( is_tax( 'post_format', 'post-format-quote' ) ) :
-                _e( 'Quotes', '_s' );
+            // elseif ( is_tax( 'post_format', 'post-format-quote' ) ) :
+            //     _e( 'Quotes', '_s' );
 
-            elseif ( is_tax( 'post_format', 'post-format-link' ) ) :
-                _e( 'Links', '_s' );
+            // elseif ( is_tax( 'post_format', 'post-format-link' ) ) :
+            //     _e( 'Links', '_s' );
 
-            elseif ( is_tax( 'post_format', 'post-format-status' ) ) :
-                _e( 'Statuses', '_s' );
+            // elseif ( is_tax( 'post_format', 'post-format-status' ) ) :
+            //     _e( 'Statuses', '_s' );
 
-            elseif ( is_tax( 'post_format', 'post-format-audio' ) ) :
-                _e( 'Audios', '_s' );
+            // elseif ( is_tax( 'post_format', 'post-format-audio' ) ) :
+            //     _e( 'Audios', '_s' );
 
-            elseif ( is_tax( 'post_format', 'post-format-chat' ) ) :
-                _e( 'Chats', '_s' );
+            // elseif ( is_tax( 'post_format', 'post-format-chat' ) ) :
+            //     _e( 'Chats', '_s' );
 
             else :
                 _e( 'Archives', '_s' );
