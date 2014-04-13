2c0af8f4e78bcd7d9930247a62e42d66768e5f78
ItsJonQ <itsjonq@gmail.com>
2014-04-13 00:21:20 -0400

    Updated archive template

--- archive.php
+++ archive.php
@@ -1,105 +1,66 @@
 <?php
 /**
- * The template for displaying Archive pages.
+ * Home
+ * @template
  *
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
- *
- * @package _s
+ * @package q_
  */
 
-get_header(); ?>
+get_header();
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+// Defining the $index for the post loop
+$index = 0;
+?>
 
-		<?php if ( have_posts() ) : ?>
+    <!-- Main Container -->
+    <div class="main-container container" id="main-container">
+        <div class="row">
 
-			<header class="page-header">
-				<h1 class="page-title">
-					<?php
-						if ( is_category() ) :
-							single_cat_title();
+            <!-- Post List Container -->
+            <section class="col-md-8 col-solo" id="post-list">
 
-						elseif ( is_tag() ) :
-							single_tag_title();
+            <?php
+            /**
+             * Getting the posts
+             */
+            if ( have_posts() ) : ?>
 
-						elseif ( is_author() ) :
-							printf( __( 'Author: %s', '_s' ), '<span class="vcard">' . get_the_author() . '</span>' );
+            <!-- Archive Page Header -->
+            <header class="page-header">
+                <!-- Archive Page Title -->
+                <h1 class="page-title">
+                    <?php q_post::archive_title(); ?>
+                </h1>
+                <?php
+                    // Show an optional term description.
+                    $term_description = term_description();
+                    if ( ! empty( $term_description ) ) :
+                        printf( '<div class="taxonomy-description">%s</div>', $term_description );
+                    endif;
+                ?>
+            </header>
+            <?php
+                // Looping through the posts
+                while ( have_posts() ) {
+                    // Defining the post info
+                    the_post();
+                    // Increasing the $index
+                    $index++;
+                    // Loading up the content template
+                    get_template_part( 'templates/content/content', 'post' );
+                }
 
-						elseif ( is_day() ) :
-							printf( __( 'Day: %s', '_s' ), '<span>' . get_the_date() . '</span>' );
+            else :
 
-						elseif ( is_month() ) :
-							printf( __( 'Month: %s', '_s' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', '_s' ) ) . '</span>' );
+                /**
+                 * No posts!
+                 */
 
-						elseif ( is_year() ) :
-							printf( __( 'Year: %s', '_s' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', '_s' ) ) . '</span>' );
+            endif; ?>
 
-						elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
-							_e( 'Asides', '_s' );
+            </section>
 
-						elseif ( is_tax( 'post_format', 'post-format-gallery' ) ) :
-							_e( 'Galleries', '_s');
+        </div>
+    </div>
 
-						elseif ( is_tax( 'post_format', 'post-format-image' ) ) :
-							_e( 'Images', '_s');
-
-						elseif ( is_tax( 'post_format', 'post-format-video' ) ) :
-							_e( 'Videos', '_s' );
-
-						elseif ( is_tax( 'post_format', 'post-format-quote' ) ) :
-							_e( 'Quotes', '_s' );
-
-						elseif ( is_tax( 'post_format', 'post-format-link' ) ) :
-							_e( 'Links', '_s' );
-
-						elseif ( is_tax( 'post_format', 'post-format-status' ) ) :
-							_e( 'Statuses', '_s' );
-
-						elseif ( is_tax( 'post_format', 'post-format-audio' ) ) :
-							_e( 'Audios', '_s' );
-
-						elseif ( is_tax( 'post_format', 'post-format-chat' ) ) :
-							_e( 'Chats', '_s' );
-
-						else :
-							_e( 'Archives', '_s' );
-
-						endif;
-					?>
-				</h1>
-				<?php
-					// Show an optional term description.
-					$term_description = term_description();
-					if ( ! empty( $term_description ) ) :
-						printf( '<div class="taxonomy-description">%s</div>', $term_description );
-					endif;
-				?>
-			</header><!-- .page-header -->
-
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php
-					/* Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
-				?>
-
-			<?php endwhile; ?>
-
-			<?php _s_paging_nav(); ?>
-
-		<?php else : ?>
-
-			<?php get_template_part( 'content', 'none' ); ?>
-
-		<?php endif; ?>
-
-		</main><!-- #main -->
-	</section><!-- #primary -->
-
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
