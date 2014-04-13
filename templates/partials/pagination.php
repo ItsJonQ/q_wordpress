fe040bf846280e27ce067b9db96299122408faf3
ItsJonQ <itsjonq@gmail.com>
2014-04-13 17:29:19 -0400

    Added button class. Updated style of pagination

--- templates/partials/pagination.php
+++ templates/partials/pagination.php
@@ -19,14 +19,14 @@
 
             <?php if ( get_next_posts_link() ) : ?>
             <!-- Previous Posts -->
-            <div class="nav-previous">
+            <div class="nav-link nav-previous button">
                 <?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', '_s' ) ); ?>
             </div>
             <?php endif; ?>
 
             <?php if ( get_previous_posts_link() ) : ?>
             <!-- Next Posts -->
-            <div class="nav-next">
+            <div class="nav-link nav-next button">
                 <?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', '_s' ) ); ?>
             </div>
             <?php endif; ?>
