b88dd0b1c2d4301aaf90e70426754b9e0406681a
ItsJonQ <itsjonq@gmail.com>
2014-04-13 00:24:03 -0400

    Created q_image class

---
+++ lib/classes/class.q_image.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * q_image
+ * This is the image class for the q_ wordpress theme
+ *
+ * @class
+ *
+ * @package q_
+ *
+ */
+
+// Defining the q_image class
+if(!class_exists('q_image')) {
+
+    class q_image {
+
+        /**
+         * init
+         * Initialization method for this class
+         *
+         * @init
+         */
+        public static function init() {
+            /**
+             * Nothin' yet!
+             */
+        }
+
+    }
+
+    // Initialize
+    // add_action( 'init' , array( 'q_image' , 'init' ) );
+
+}
