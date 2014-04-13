1e5f91ccb53834008dac4fb7b9732d5b419f4e8d
ItsJonQ <itsjonq@gmail.com>
2014-04-13 00:21:42 -0400

    Added the archive_title method into q_post, which came stock with Underscores WP'

--- lib/classes/class.q_post.php
+++ lib/classes/class.q_post.php
@@ -27,6 +27,70 @@ if(!class_exists('q_post')) {
         }
 
         /**
+         * archive_title
+         * This is the archive title template that came stock with the
+         * Wordpress Underscores theme.
+         *
+         * _e is a Built in WP class (not exclusive to Underscores)
+         * @reference http://codex.wordpress.org/Function_Reference/_e
+         *
+         * @return [ html/text ]     Echo's the archive page's title
+         */
+        public static function archive_title() {
+
+            if ( is_category() ) :
+                single_cat_title();
+
+            elseif ( is_tag() ) :
+                single_tag_title();
+
+            elseif ( is_author() ) :
+                printf( __( 'Author: %s', '_s' ), '<span class="vcard">' . get_the_author() . '</span>' );
+
+            elseif ( is_day() ) :
+                printf( __( 'Day: %s', '_s' ), '<span>' . get_the_date() . '</span>' );
+
+            elseif ( is_month() ) :
+                printf( __( 'Month: %s', '_s' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', '_s' ) ) . '</span>' );
+
+            elseif ( is_year() ) :
+                printf( __( 'Year: %s', '_s' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', '_s' ) ) . '</span>' );
+
+            elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
+                _e( 'Asides', '_s' );
+
+            elseif ( is_tax( 'post_format', 'post-format-gallery' ) ) :
+                _e( 'Galleries', '_s');
+
+            elseif ( is_tax( 'post_format', 'post-format-image' ) ) :
+                _e( 'Images', '_s');
+
+            elseif ( is_tax( 'post_format', 'post-format-video' ) ) :
+                _e( 'Videos', '_s' );
+
+            elseif ( is_tax( 'post_format', 'post-format-quote' ) ) :
+                _e( 'Quotes', '_s' );
+
+            elseif ( is_tax( 'post_format', 'post-format-link' ) ) :
+                _e( 'Links', '_s' );
+
+            elseif ( is_tax( 'post_format', 'post-format-status' ) ) :
+                _e( 'Statuses', '_s' );
+
+            elseif ( is_tax( 'post_format', 'post-format-audio' ) ) :
+                _e( 'Audios', '_s' );
+
+            elseif ( is_tax( 'post_format', 'post-format-chat' ) ) :
+                _e( 'Chats', '_s' );
+
+            else :
+                _e( 'Archives', '_s' );
+
+            endif;
+
+        }
+
+        /**
          * meta
          * @param  [number]  $id    [ Inserting the post's ID ]
          * @param  boolean $echo    [ Determing whether to return or echo the $output ]
