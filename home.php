0526d09b2ebcf4e720242439ae8a2a5b8d61c7fd
ItsJonQ <itsjonq@gmail.com>
2014-04-12 20:11:54 -0400

    Added indentation to home.php

--- home.php
+++ home.php
@@ -8,37 +8,37 @@
 
 get_header(); ?>
 
-<!-- Main Container -->
-<div class="main-container container" id="main-container">
-    <div class="row">
+    <!-- Main Container -->
+    <div class="main-container container" id="main-container">
+        <div class="row">
 
-        <!-- Post List Container -->
-        <section class="col-md-8 col-solo" id="post-list">
+            <!-- Post List Container -->
+            <section class="col-md-8 col-solo" id="post-list">
 
-        <?php
-        /**
-         * Getting the posts
-         */
-        if ( have_posts() ) :
-            // Looping through the posts
-            while ( have_posts() ) {
-                // Defining the post info
-                the_post();
-                // Loading up the content template
-                get_template_part( 'templates/content/content', 'post' );
-            }
-
-        else :
-
+            <?php
             /**
-             * No posts!
+             * Getting the posts
              */
+            if ( have_posts() ) :
+                // Looping through the posts
+                while ( have_posts() ) {
+                    // Defining the post info
+                    the_post();
+                    // Loading up the content template
+                    get_template_part( 'templates/content/content', 'post' );
+                }
 
-        endif; ?>
+            else :
 
-        </section>
+                /**
+                 * No posts!
+                 */
 
+            endif; ?>
+
+            </section>
+
+        </div>
     </div>
-</div>
 
 <?php get_footer(); ?>
