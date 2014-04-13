e10272dc407a4d4c7200345aee97c3d60d71ce96
ItsJonQ <itsjonq@gmail.com>
2014-04-12 23:54:41 -0400

    Created the title method for q_post that outputs the post's headline with a SEO'ed header tag

--- lib/classes/class.q_post.php
+++ lib/classes/class.q_post.php
@@ -26,6 +26,44 @@ if(!class_exists('q_post')) {
              */
         }
 
+        /**
+         * title
+         * Generating the title for the post
+         * @param  integer $index [ Creates an <h1> tag if the index is 1 (first post) ]
+         * @param  boolean $echo  [ If false, return the $output instead of echo'ing it ]
+         * @return [ html ]       [ the title wrapped in a header tag ]
+         */
+        public static function title( $index = 2, $echo = true ) {
+            // Defining the header tag
+            $header_tag = 'h2';
+
+            // Change the $header_tag to h1 if the $index is 1
+            if( $index === 1 ) {
+                $header_tag = 'h1';
+            }
+
+            // Defining the $title
+            $title = get_the_title();
+            // Defining the $permalink
+            $permalink = get_permalink();
+
+            // Adjusting the title if the page is NOT singular
+            if( !is_singular() ) {
+                $title = '<a href="'.$permalink.'" title="Read '.$title.'" rel="bookmark">'.$title.'</a>';
+            }
+            // Creating the headline
+            $output = '<'.$header_tag.'>'.$title.'</'.$header_tag.'/>';
+
+            // Return / echo the $output
+            if( $echo === true ) {
+                echo $output;
+            } else {
+                return $output;
+            }
+        }
+
+
+
     }
 
     // Initialize
