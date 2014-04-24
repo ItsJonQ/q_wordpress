5413f9e3f42463de2e42dadb2042548e612d29c0
ItsJonQ <itsjonq@gmail.com>
2014-04-24 17:57:57 -0400

    Added additional text + social link and icons in the footer

--- footer.php
+++ footer.php
@@ -10,6 +10,15 @@
     </div><!-- / #content -->
     <!-- Footer Container -->
     <footer class="col-md-8 col-solo site-footer" id="main-footer" role="contentinfo">
+        <div class="text-center socials">
+            <small class="text-smaller">Oh snap! You made it to the footer, <a href="https://www.youtube.com/watch?v=Rp7jp5H-xWU" target="_blank">like a boss</a></small>.
+            <br>
+            <small class="text-smaller">Find me on</small>
+            <br>
+            <a href="https://twitter.com/itsJonQ" target="_blank" class="icon" title="Twitter"><i class="icon-twitter"></i></a>
+            <a href="https://github.com/ItsJonQ/" target="_blank" class="icon" title="Github"><i class="icon-github"></i></a>
+        </div>
+
         <div class="site-info">
             Built by <strong><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">Q</a></strong> . 2014.
         </div><!-- / .site-info -->
